61ca4906e0a736ae8e94242cdaf7b1f9ec16d60d
Artur <103675840+Ar2r906@users.noreply.github.com>
2024-02-28 11:17:15 +0500

    Update App.vue

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
-    
-  </div>
+  <div class="header">
+      <div class="image">
+        <router-link to="/home" ><img src="./assets/Logotype.svg" alt="logotype"></router-link>
+      </div>
+      <nav>
+        <router-link to="/auth" >Войти</router-link>
+        <router-link to="/registration" >Зарегистрироваться</router-link>
+      </nav>
+  </div>  
 </template>
 
 <script>
-
   export default {
     
   }
@@ -15,5 +20,26 @@
   * {
     padding: 0;
     margin: 0;
+  }
+  .header {
+    display: flex;
+    flex-direction: column;
+    width: auto;
+    width: auto;
+  }
+  .header img {
+    padding: 5dvh 0 0 5dvw;
+  }
+  .image {
+  }
+  nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    column-gap: 10dvw;
+    padding-top: 0;
+  }
+  .router-link {
+    text-decoration: none;
   }
 </style>
