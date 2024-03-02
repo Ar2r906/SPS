a551b45e90a5407ab0d6b8405d343b22b74c02b8
Artur <103675840+Ar2r906@users.noreply.github.com>
2024-03-02 12:30:46 +0500

    NavBar.vue
    Создал компонент NavBar и немного стилизовал

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -1,56 +1,16 @@
 <template>
-  <div class="home-nav">
-    <div class="logo">
-      <a href="/home"><img src="./assets/Logotype.svg" alt="logotype"></a>
-    </div>
-    <nav>
-      <router-link to="/login">Вход</router-link>
-      <router-link to="/register">Регистрация</router-link>
-      <router-link to="/#">Мероприятия</router-link>
-      <router-link to="/#">Партнерам</router-link>
-      <router-link to="/#">Контакты</router-link>
-    </nav> 
-    <router-view />
-  </div>
+  <NavBar />
 </template>
-hello world
+
+<script>
+  import NavBar from './components/NavBar.vue'
+  export default {
+    components: {
+      NavBar,
+    }
+  }
+</script>
+
 <style>
-  @font-face {
-    font-family: "JetBrains Mono";
-    src: url(./fonts/JetBrainsMonoNL-Regular.ttf);
-  }
-  body {
-    padding: 0;
-    margin: 0;
-    font-family: "JetBrains Mono";
-  }
-  .home-nav {
-    height: 80dvh;
-    background-color: #1B1C21;
-    display: flex;
-    flex-direction: row;
-    padding-top: 2dvh;
-  }
-  .logo {
-    justify-content: start; 
-    padding-left: 2dvw;
-  }
-  nav { 
-    display: flex;
-    flex-direction: row;
-    column-gap: 3dvw;
-    justify-content: flex-end;
-    margin-left: auto;
-    padding-right: 2dvw;
-  }
-  nav a {
-    font-family: "JetBrains Mono";
-    text-decoration: none;
-    color: #D9D9D9;
-    font-size: 2dvh;
-    font-weight: 500;
-  }
-  nav a:hover {
-    color: #402FFF;
-  }
+  
 </style>
--- Front/src/views/Login.vue
+++ Front/src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
-        login
+    <div class="login">
+        
     </div>
 </template>
