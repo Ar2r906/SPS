70f0eee5366e84eae323850f11978ac0cfb2593a
Artur <103675840+Ar2r906@users.noreply.github.com>
2024-03-03 15:45:54 +0500

    .

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  
+  <NavBar />
 </template>
 
 <script>
-
+import NavBar from './components/NavBar.vue'
+export default {
+    components: {
+        NavBar,
+    },
+}
 </script>
 
 <style>
