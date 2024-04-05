30811bfc208fdcbd0d8384dfd7b2fe2c47a82a81
Валькирия <majskaja_maria@mail.ru>
2024-04-06 01:43:44 +0500

    потыкалась в гл

---
+++ Front/src/components/Buttons.vue
@@ -0,0 +1,39 @@
+<template>
+    <input class="left" type="button" name="left" style="color: #402FFF;" value="&#5130;" />
+    <input class="right" type="button" name="right" style="color: #402FFF;" value="&#5125;" />
+    <!-- <input class="left" type="button" name="left" style="color: #402FFF;" value="&#9668;" /> -->
+    <!-- <input class="right" type="button" name="right" style="color: #402FFF;" value="&#9658;" /> -->
+</template>
+
+<script>
+export default {
+    name: 'Buttons',
+}
+</script>
+
+<style>
+.left {
+  position: absolute;
+  background: white;
+  font-family: JetBrains Mono;
+  text-color: #D9D9D9;
+  font-size: 20px;
+  width: 55px;
+  height: 55px;
+  border: 2px solid #402FFF;
+  border-radius: 50px;
+  margin-left: 15px;
+}
+.right {
+  position: absolute;
+  background: white;
+  font-family: JetBrains Mono;
+  text-color: #D9D9D9;
+  font-size: 20px;
+  width: 55px;
+  height: 55px;
+  border: 2px solid #402FFF;
+  border-radius: 50px;
+  margin-left: 1820px;
+}
+</style>
--- Front/src/views/Home.vue
+++ Front/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <News />
   <Events/>
+  <Partners2/>
   <Contacts/>
 </template>
 
@@ -8,6 +9,8 @@
 import News from '../components/News'
 import Events from '../components/Events.vue'
 import Contacts from '../components/Contacts.vue'
+import Buttons from '../components/Buttons.vue'
+import Partners2 from '../components/Partners2.vue'
 
 export default {
   name: 'Home',
@@ -15,6 +18,8 @@ export default {
     News,
     Events,
     Contacts,
+    Buttons,
+    Partners2,
   },
 }
 </script>
