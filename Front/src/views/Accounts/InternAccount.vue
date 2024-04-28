cd44116311ed25962ed09cad247d6142904e7869
Валькирия <majskaja_maria@mail.ru>
2024-04-29 02:30:36 +0500

    Update InternAccount.vue

--- Front/src/views/Accounts/InternAccount.vue
+++ Front/src/views/Accounts/InternAccount.vue
@@ -1,13 +1,59 @@
 <template>
-    <div>
-        Intern account
+<form id="Intern" action="" method="">
+  <div>
+   <label>
+    Учебное заведение:</label>
+    <input class='pole5'
+      type="text"
+      name="educational"
+      id="educational"
+    >
     </div>
+  <div>
+  <label>
+    Группа/Класс: </label>
+    <input class='pole5'
+      type="text"
+      name="group"
+      id="group"
+    >
+  </div>
+  <div>
+  <label>
+    Почта: </label>
+    <input class='pole5' type="email" name="email">
+  </div>
+  <div>
+  <label>
+    Телефон: </label>
+    <input class='pole5'
+      type="text"
+      name="phone"
+      id="phone"
+    >
+    </div>
+  </form>
+  <button class="send1">&#8249;Пройти тестирование&#8250;</button>
 </template>
 
 <script>
-
 </script>
 
 <style>
-
+label{
+  font-family: JetBrains Mono;
+  font-size: 15px;
+  font-weight: 700;
+}
+  .pole5{
+      height: 30px;
+      width: 250px;
+      margin-top:10px;
+      border: 1px solid;
+      border-radius: 10px;
+      font-size: 20px;
+      background: #D9D9D9;
+      font-family: JetBrains Mono;
+      text-align: center;
+    }
 </style>
