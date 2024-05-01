bf43aa03003943b4d4587e66464a61a8bc81197a
Artur_Ibragimov <103675840+Ar2r906@users.noreply.github.com>
2024-05-01 13:40:42 +0500

    internAccount

--- Front/src/views/Accounts/InternAccount.vue
+++ Front/src/views/Accounts/InternAccount.vue
@@ -1,47 +1,94 @@
 <template>
-  <div id="kvadrat_0"></div>
-  <button class="photo">Изменить</button>
+  <div class="main">
+    <div class="block-photo">
+      <div class="block-photo image">
+        <img src="../../assets/temp/Image_profile.svg" alt="image-profile">
+      </div>
+      <button class="block-photo button">Изменить</button>
+    </div>
+    <div class="block-info">
+      <form @submit.prevent="">
+        <label>
+          Учебное заведение:
+          <input type="text" v-model="educational_envy">
+        </label>
+
+        <label>
+          Группа/класс:
+          <input type="text" v-model="group">
+        </label> 
+        
+        <label>
+          Почта:
+          <input type="text" v-model="email">
+        </label>
+        
+        <label>
+          Телефон:
+          <input type="text" v-model="telephone">
+        </label>
+        
+      </form>
+    </div>
+  </div>
   <button class="send1">&#8249;Пройти тестирование&#8250;</button>
 </template>
 
 <script>
 </script>
 
-<style>
-div#kvadrat_0 {
+<style scoped>
+.main {
+  display: flex;
   width: 90%;
-  height: 500px;
-  border: 5px solid #D9D9D9;
+  height: 60dvh;
   background: white;
+  border: 4px solid #D9D9D9;
   border-radius: 25px;
   position: absolute;
-  top: 80%;
-  left: 5%;
+  margin: 5% 5%;
 }
+form {
+  margin-top: 5dvh;
+  margin-left: 10dvw;
+}
+
+
 .send1{
   position: absolute;
-  top: 128%;
-  left: 10%;
+  top: 135.5%;
+  left: 7%;
 }
-.photo{
-    /* margin:auto; */
-    margin-top: 40px;
-    margin-left: 100px;
-    width: 150px;
-    height: 35px;
-    font-family: 'JetBrains Mono';
-    color: #D9D9D9;
-    border: 1px solid #402FFF;
-    border-radius: 25px;
-    background: #402FFF;
-    font-size: 15px;
-    position: absolute;
-    top: 100%;
-    left: 7%;
-  }
-.photo:hover{
-    border: 1px solid #000;
-    background-color: #000;
-  }
-
+.send1:hover {
+  border: 1px solid #000;
+  background-color: #000;
+}
+.button{
+  /* margin:auto; */
+  /* margin-top: 40px;
+  margin-left: 100px; */
+  width: 150px;
+  height: 35px;
+  font-family: 'JetBrains Mono';
+  color: #D9D9D9;
+  border: 1px solid #402FFF;
+  border-radius: 15px;
+  background: #402FFF;
+  font-size: 15px;
+  position: absolute;
+  left: 9.5%;
+  margin-top: 3dvh;
+  /* top: 100%;
+  left: 7%; */
+}
+.button:hover{
+  border: 1px solid #000;
+  background-color: #000;
+}
+.block-photo {
+  margin-left: 2dvw;
+}
+.image {
+  padding-top: 5dvh;
+}
 </style>
