857e2fbdd860635299a59b4c8036fd2a6225bacd
Валькирия <majskaja_maria@mail.ru>
2024-05-06 02:56:49 +0500

    Update HeadCoachAccount.vue

--- Front/src/views/Accounts/HeadCoachAccount.vue
+++ Front/src/views/Accounts/HeadCoachAccount.vue
@@ -1,13 +1,122 @@
 <template>
-    <div>
-        Head coach account
+  <div class="main">
+    <div class="block-photo">
+      <div class="block-photo image">
+        <img src="../../assets/temp/Image_profile.svg" alt="image-profile">
+      </div>
+      <button class="block-photo button">Изменить</button>
     </div>
+
+    <div class="block-info">
+      <form @submit.prevent="">
+        <div>
+          <label>Фамилия Имя Отчество</label>
+        </div>
+        <div>
+        <label>
+          Почта:
+          <input type="text" v-model="email">
+        </label>
+        </div>
+        <div>
+        <label>
+          Телефон:
+          <input type="text" v-model="phone">
+        </label> 
+        </div>
+        <div>
+        <label>
+          О себе:
+          <input type="text" v-model="about">
+        </label>
+        </div>
+      </form>
+    </div>
+    </div>
+  <div class="main2">
+    <div>
+        <label>&#8249;Мои команды&#8250;</label>
+    </div>
+    </div>
+
 </template>
 
 <script>
-
 </script>
 
-<style>
+<style scoped>
+.main, .main2 {
+  display: flex;
+  width: 90%;
+  height: 60dvh;
+  background: white;
+  border: 7px solid #D9D9D9;
+  border-radius: 25px;
+  margin: 5% 5%;
+}
+
+
+form {
+  margin-top: 5dvh;
+  margin-left: 10dvw;
+  width: 750px; /* ширина формы */
+}
+.main2 label{
+  margin-left: 38dvw;
+  font-size: 25px;
+}
+
+label{
+  font-family: 'JetBrains Mono';
+  font-size: 20px;
+  font-weight:bold;
+  display: flex;
+  justify-content: space-between;
+  padding: 25px;
+}
+input{
+  width: 400px;
+  height: 33px;
+  font-family: 'JetBrains Mono';
+  font-size: 17px;
+  border-radius: 10px;
+  border: 1px solid black;
+  background: #D9D9D9;
+  float: right;  /* выравнивание по правому краю */ 
+  padding-left: 15px; /* отступ текста в поле */ 
+}
+.send1{
+  position: absolute;
+  top: 134%;
+  left: 7%;
+}
+
+.send1:hover {
+  border: 1px solid #000;
+  background-color: #000;
+}
+.button{
+  width: 150px;
+  height: 35px;
+  font-family: 'JetBrains Mono';
+  color: #D9D9D9;
+  border: 1px solid #402FFF;
+  border-radius: 15px;
+  background: #402FFF;
+  font-size: 15px;
+  position: absolute;
+  left: 14.5%;
+  margin-top: 2dvh;
+}
+.button:hover{
+  border: 1px solid #000;
+  background-color: #000;
+}
+.block-photo {
+  margin-left: 2dvw;
+}
+.image {
+  padding-top: 5dvh;
+}
 
 </style>
