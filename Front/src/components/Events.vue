ecf1cfa330408e0c8838f874eaa64083b3f9633e
Валькирия <majskaja_maria@mail.ru>
2024-06-09 16:39:02 +0500

    Update Events.vue

--- Front/src/components/Events.vue
+++ Front/src/components/Events.vue
@@ -1,82 +1,16 @@
 <template>
-    <div class="windows">
-        <div class="window-title">
-            <h2>&lt;Мероприятия&gt;</h2>
-            <!--<h3>&lt;Ближайшие&gt;</h3>-->
-        </div>
-        <div class="windows-blocks">
-        <div class="window-inform">
-            <div class="inform-main">
-                <div class="main-image">
-                    <img src="../assets/image_in_news.svg" alt="test image" style="width:100%">
-                </div>
-                <div class="main-title">
-                    <h2>Заголовок</h2>
-                </div>
-                <div class="main-text">
-                    <p>Lorem Ipsum - это текст-”рыба”, часто используемый в печати и веб-дизайне. Lorem Ipsum является стандартной “рыбой”.</p>
-                </div>
-                <div class="main-date">
-                    <time datetime="18.03.2024">18.03.2024</time>
-                </div>
-                <div class="more_info_button">
-                  <button class="form form-button">Узнать подробнее</button>
-                </div>
-            </div></div>
-
-            <div class="window-inform">
-            <div class="inform-main">
-                <div class="main-image">
-                    <img src="../assets/image_in_news.svg" alt="test image" style="width:100%">
-                </div>
-                <div class="main-title">
-                    <h2>Заголовок</h2>
-                </div>
-                <div class="main-text">
-                    <p>Lorem Ipsum - это текст-”рыба”, часто используемый в печати и веб-дизайне. Lorem Ipsum является стандартной “рыбой”.</p>
-                </div>
-                <div class="main-date">
-                    <time datetime="18.03.2024">18.03.2024</time>
-                </div>
-                <div class="more_info_button">
-                  <button class="form form-button">Узнать подробнее</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="window-inform">
-            <div class="inform-main">
-                <div class="main-image">
-                    <img src="../assets/image_in_news.svg" alt="test image" style="width:100%">
-                </div>
-                <div class="main-title">
-                    <h2>Заголовок</h2>
-                </div>
-                <div class="main-text">
-                    <p>Lorem Ipsum - это текст-”рыба”, часто используемый в печати и веб-дизайне. Lorem Ipsum является стандартной “рыбой”.</p>
-                </div>
-                <div class="main-date">
-                    <time datetime="18.03.2024">18.03.2024</time>
-                </div>
-                <div class="more_info_button">
-                  <button class="form form-button">Узнать подробнее</button>
-                </div>
-            </div></div>
-      </div>
-  </div>
-<div>
-
+<h1>&lt;Мероприятия&gt;</h1>
 <table>
     <tr>
-        <td>Дата</td>
-        <td>Название</td>
-        <td>Место</td>
-        <td>Дисциплина</td>
-        <td>Участники</td>
+        <td><u>Дата</u></td>
+        <td><u>Название</u></td>
+        <td><u>Место</u></td>
+        <td><u>Дисциплина</u></td>
+        <td><u>Участники</u></td>
     </tr>
     <tr>
         <td>27-29 июня</td>
-        <td><a href="#">Всероссийские соревнования по спортивному программированию</a></td>
+        <td><a href="#"  style="text-decoration: none; color:black;">Всероссийские соревнования по спортивному программированию</a></td>
         <td>Г. Оренбург</td>
         <td>Программирование робототехники</td>
         <td>Студенты</td>
@@ -84,7 +18,7 @@
 
     <tr>
         <td>1-3 августа</td>
-        <td><a href="#">Всероссийские соревнования по спортивному программированию среди студентов</a></td>
+        <td><a href="#"  style="text-decoration: none; color:black;">Всероссийские соревнования по спортивному программированию среди студентов</a></td>
         <td>Г. Москва, РЭУ им. Г.В. Плеханова</td>
         <td></td>
         <td>Студенты</td>
@@ -93,83 +27,30 @@
 
     <tr>
         <td>1-3 августа</td>
-        <td><a href="#">Всероссийские соревнования по спортивному программированию</a></td>
+        <td><a href="#"  style="text-decoration: none; color:black;">Всероссийские соревнования по спортивному программированию</a></td>
         <td>Г. Москва, РЭУ им. Г.В. Плеханова</td>
         <td></td>
         <td>Мужчины и женщины</td>
-
     </tr>
+
+    <tr>
+        <td>27-31 августа</td>
+        <td><a href="#"  style="text-decoration: none; color:black;">Всероссийские соревнования среди студентов по спортивному программированию "Кубок будущего"</a></td>
+        <td>Г. Калуга, KFL Arena</td>
+        <td></td>
+        <td>Студенты</td>
+    </tr>
+
+    <tr>
+        <td>13-15 сентября</td>
+        <td><a href="#"  style="text-decoration: none; color:black;">Всероссийские соревнования по спортивному программированию</a></td>
+        <td>Г. Пермь, Пермский край</td>
+        <td>Программирование систем информационной безопасности</td>
+        <td>Мужчины и женщины от 16 лет</td>
+    </tr>
+
 </table>
-</div>
-
-
-  <!-- 
-      <div class="windows">
-        <div class="window-title">
-            <h3>&lt;Прошедшие&gt;</h3>
-        </div>
-        <div class="windows-blocks">
-        <div class="window-inform">
-            <div class="inform-main">
-                <div class="main-image">
-                    <img src="../assets/image_in_news.svg" alt="test image" style="width:100%">
-                </div>
-                <div class="main-title">
-                    <h2>Заголовок</h2>
-                </div>
-                <div class="main-text">
-                    <p>Lorem Ipsum - это текст-”рыба”, часто используемый в печати и веб-дизайне. Lorem Ipsum является стандартной “рыбой”.</p>
-                </div>
-                <div class="main-date">
-                    <time datetime="18.03.2024">18.03.2024</time>
-                </div>
-                <div class="more-info-button">
-                  <button class="form form-button">Узнать подробнее</button>
-                </div>
-          </div>
-        </div>
-
-        <div class="window-inform">
-            <div class="inform-main">
-                <div class="main-image">
-                    <img src="../assets/image_in_news.svg" alt="test image" style="width:100%">
-                </div>
-                <div class="main-title">
-                    <h2>Заголовок</h2>
-                </div>
-                <div class="main-text">
-                    <p>Lorem Ipsum - это текст-”рыба”, часто используемый в печати и веб-дизайне. Lorem Ipsum является стандартной “рыбой”.</p>
-                </div>
-                <div class="main-date">
-                    <time datetime="18.03.2024">18.03.2024</time>
-                </div>
-                <div class="more-info-button">
-                  <button class="form form-button">Узнать подробнее</button>
-                </div>
-          </div>
-        </div>
-
-        <div class="window-inform">
-            <div class="inform-main">
-                <div class="main-image">
-                    <img src="../assets/image_in_news.svg" alt="test image" style="width:100%">
-                </div>
-                <div class="main-title">
-                    <h2>Заголовок</h2>
-                </div>
-                <div class="main-text">
-                    <p>Lorem Ipsum - это текст-”рыба”, часто используемый в печати и веб-дизайне. Lorem Ipsum является стандартной “рыбой”.</p>
-                </div>
-                <div class="main-date">
-                    <time datetime="18.03.2024">18.03.2024</time>
-                </div>
-                <div class="more-info-button">
-                  <button class="form form-button">Узнать подробнее</button>
-                </div>
-          </div>
-        </div></div>
-</div> -->
-  <hr>
+<hr>
 </template>
 
 <script>
@@ -183,101 +64,29 @@ table{
     width: 95%;
     text-align: center;
     margin-left: 3%;
-    margin-top: 6%;
+    margin-top: 10px;
 }
 td {
   text-align: center;
 	padding: 10px;
     font-family: JetBrainsMonoNL;
-    font-size: 23px;
+    font-size: 22px;
+    font-weight: bold;
 }
  th {
   text-align: center;
   padding: 10px;
  }
 
-.windows {
-    display: flex;
-    flex-direction: column;
-    row-gap: 5dvh;
-    width: 96dvw;
-    margin: 0 2dvw;
-    height: auto;
-}
-.window-title {
-    padding-top: 4dvh;
-}
-.window-title h1 {
-    font-size: 3rem;
-}
-.windows-blocks {
-    display: flex;
-    flex-direction: row;
-    column-gap: 3dvw;
-}
-.window-inform {
-    display: flex;
-    flex-direction: column;
-    background-color: #2D2E37;
-    height: 56dvh;
-    width: 30dvw;
-    justify-content: center;
-    align-items: center;
-}
-.inform-main {
-    height: 46dvh;
-    width: 26dvw;
-    background-color: #000000;
-    color: #fff;
-}
-.form {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding-top: 5dvh;
-  padding-bottom: 5dvh;
-}
 h1 {
-  padding-bottom: 5dvh;
-}
-.form-input {
-  padding: 1dvh 1dvw;
-  margin: 2dvh auto;
-  width: 20dvw;
-  color: #000;
-  background-color: #D9D9D9;
-  border: 1px solid #000;
-  border-radius: 8px;
-}
-.form-button {
-  margin: 1dvh 0;
-  padding: 1dvh 1dvw;
-  background-color: #402FFF;
-  border: #1b1c21 1px solid;
-  border-radius: 8px;
-  color: #fff;
-  width: 100%;
-}
-.form-button:hover {
-  background-color: #1b1c21;
-  color: #fff;
-}
-.text a{
-  /* text-decoration: none; */
-  color: #000;
-}
-.text a:hover {
-  color: #402FFF;
+  margin: 35px 0px 0px 70px;
 }
 hr {
-  	margin: -30px auto 10px;
+  	margin: 35px auto 10px;
 	padding: 0;
 	height: 50px;
 	border: none;
 	border-bottom: 2px solid #1f1209;
 	width: 95%;
 }
-
-
 </style>
