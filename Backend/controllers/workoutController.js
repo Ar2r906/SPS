bbfb609ab18623d428cfb2c871d49e350207e74c
Валькирия <majskaja_maria@mail.ru>
2024-06-11 23:55:01 +0500

    Update workoutController.js

--- Backend/controllers/workoutController.js
+++ Backend/controllers/workoutController.js
@@ -11,9 +11,9 @@ exports.getWorkouts = async (req, res) => {
 };
 
 exports.createWorkout = async (req, res) => {
-    const { title, description, date } = req.body;
+    const { title, time, time2, date, complexity } = req.body;
     try {
-        const workout = await workout.create({ title, description, date, id: req.user.id });
+        const workout = await workout.create({ title, time, time2, date, complexity, id: req.user.id });
         res.status(201).json(workout);
     } catch (error) {
         res.status(400).json({ message: error.message });
