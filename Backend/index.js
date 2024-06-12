d6980f5cce61fe8447bf7e8ca62f077f3acfebe7
Валькирия <majskaja_maria@mail.ru>
2024-06-13 00:46:08 +0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,6 +13,8 @@ const Post = require('./models/posts');
 const posts_routes = require('./routes/posts')
 const auths_routes = require('./routes/auths')
 const users_routes = require('./routes/users')
+const workoutController = require('./controllers/workoutController');
+
 
 const { error } = require('console')
 const bodyParser = require('body-parser')
@@ -25,6 +27,7 @@ app.use(bodyParser.json());
 app.use('/api/auth', auths_routes)
 app.use('/api/users', users_routes)
 app.use('/api/posts', posts_routes)
+app.get('/api/workouts', workoutController.getWorkouts);
 
 async function deleteOldNews(days) {
     const date = new Date();
@@ -51,6 +54,7 @@ cron.schedule('0 0 * * *', () => {
 cron.schedule('0 0 * * *', () => {
     fetchNews('https://fsp-russia.com/'); // Получение новостей каждый час
 });
+
 
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}`);
