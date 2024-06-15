9c3f7deab20eecba4f578bd03b9b011550781784
Валькирия <majskaja_maria@mail.ru>
2024-06-16 02:43:13 +0500

    Create event.js

---
+++ Backend/models/event.js
@@ -0,0 +1,32 @@
+const { sequelize } = require('../connection')
+const { DataTypes } = require('sequelize')
+
+const event = sequelize.define(
+    'event', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.TEXT,
+        },
+        date: {
+            type: DataTypes.DATE,
+        },
+        discipline: {
+            type: DataTypes.TIME,
+        },
+
+    },
+    { 
+        tableName: 'event',
+        timestamps: false,
+    },
+);
+
+async function getEvent() {
+    await event.sync()
+    console.log('Синхронизация с Event выполнена')
+}
+module.exports = { event, getEvent}
