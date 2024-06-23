35adbfd577cbc0331f283abfba5ed5e9821edd57
Валькирия <majskaja_maria@mail.ru>
2024-06-23 14:47:27 +0500

    Update user.js

--- Front/src/store/user.js
+++ Front/src/store/user.js
@@ -16,6 +16,21 @@ export default {
             const user = await instance.get('/api/users')
             if(user) return commit('setUser', user.data)
             console.log(user.message);
+        },
+        async fetchUserDetails({ commit }) {
+            try {
+                const accessToken = localStorage.getItem('accessToken');
+                const response = await fetch('http://localhost:3000/api/users', {
+                    headers: {
+                        'x-access-token': accessToken
+                    }
+                });
+                if (!response.ok) throw new Error('Ошибка запроса данных пользователя');
+                const userData = await response.json();
+                commit('setUser', userData);
+            } catch (error) {
+                console.error('Не удалось получить данные пользователя:', error);
+            }
         }
     }
 }
